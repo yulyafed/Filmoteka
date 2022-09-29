c46e60bccb3918e74c43484501978aaabf29cf7d
Alexandr Hetmanenko <fsdeveloper.alex@gmail.com>
2022-09-30 00:04:03 +0300

    add-btn-to-top

---
+++ src/js/component/btn-to-top.js
@@ -0,0 +1,25 @@
+const goToTop = document.querySelector('.btn-to-top');
+
+export function onTopBallon() {
+  window.addEventListener('scroll', trackScroll);
+  goToTop.addEventListener('click', backToTop);
+
+  function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goToTop.classList.add('btn-to-top-show');
+    }
+    if (scrolled < coords) {
+      goToTop.classList.remove('btn-to-top-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -100);
+      setTimeout(backToTop, 10);
+    }
+  }
+}
