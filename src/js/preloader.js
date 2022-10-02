f7877b3ee5d41350dc0c53b24872aa677a987b72
Alina Proskurnitska <proskur1987@gmail.com>
2022-10-02 20:34:06 +0300

    fix-scss

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -7,3 +7,28 @@ window.addEventListener('load', () => {
         preloader.remove();
     }, 1000);
 });
+
+
+
+
+
+//  const loaderFromModal = document.querySelector('.preloader');
+
+// // window.addEventListener('load', () => {
+// //     preloader.classList.add('preloader--hide');
+// //     setTimeout(() => {
+// //         preloader.remove();
+// //     }, 1000);
+// // });
+
+// function hideLoaderFromModal() {
+//     loaderFromModal.classList.add('preloader--hide');
+// }
+
+// function showLoaderFromModal() {
+//     loaderFromModal.classList.remove('preloader--hide');
+// }
+
+//  setTimeout(() => {
+//      hideLoaderFromModal();
+//     }, 1000);
