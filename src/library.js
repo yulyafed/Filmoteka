ea15e0883ee4449ca0b566e6680f32f13a75d3cb
NazarBezpaliuk <nazardxvx@gmail.com>
2022-10-04 13:04:42 +0300

    modal-upgrade

--- src/library.js
+++ src/library.js
@@ -1,8 +1,8 @@
 import { refs } from './js/refs';
 import './js/AuthenticationService';
 import { onTopBallon } from './js/component/btn-to-top';
-import './js/day-night';
 import './js/teamModal';
 import './js/getFromStorage';
 import './js/set-to-watched';
 import './js/ApiService';
+import './js/day-night';
